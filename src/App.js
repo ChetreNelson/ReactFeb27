60dfc04f8c529dc87cef824536b8bd6da3efad67
ChetreNelson <128913141+ChetreNelson@users.noreply.github.com>
2024-02-27 09:57:39 +0545

    props and usestate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./components/UseStateExample";
+import UseStateExample from "./components/UseStateExample";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UseStateExample name="ram" />
+      <UseStateExample name="shyam" />
+      <h1>hello world</h1>
+      <p>hey </p>
     </div>
   );
 }
