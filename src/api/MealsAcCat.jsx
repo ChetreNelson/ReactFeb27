46429aea04db43c05a9d935f1ecbe5d01f931023
ChetreNelson <128913141+ChetreNelson@users.noreply.github.com>
2024-03-06 23:33:24 +0545

    React router dom

--- src/api/MealsAcCat.jsx
+++ src/api/MealsAcCat.jsx
@@ -1,12 +1,68 @@
-import React from 'react'
-
+import React, { useCallback, useEffect, useState } from "react";
+import RecipeAPI from "./RecipeApi";
+import { useNavigate, useParams } from "react-router-dom";
+const { API2 } = RecipeAPI;
 function MealsAcCat() {
+  const [getprops, setGetProps] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const { categoryName } = useParams();
+  const Navigate = useNavigate();
+
+  const getApis = useCallback(async () => {
+    try {
+      setLoading(true);
+      const resp = await API2.get(`?c=${categoryName}`);
+      setGetProps(resp.data.meals);
+      setLoading(false);
+    } catch (e) {}
+  }, [categoryName]);
+
+  useEffect(() => {
+    getApis();
+  }, [getApis]);
+
+  const goBack = () => {
+    Navigate(-1);
+  };
   return (
-    <div>
-      hello world
-      
+    <div className="m-2 px-3">
+      {isLoading && (
+        <div className="flex items-center justify-center h-screen">
+          <p className="text-xl font-bold">Loading...</p>
+        </div>
+      )}
+      <button
+        onClick={goBack}
+        className="bg-gray-300 hover:bg-gray-400 text-gray-800 
+        font-bold py-3 px-6 mx-4 rounded-l"
+      >
+        GO BACK
+      </button>
+
+      <div className="grid  md:grid-cols-6 gap-2">
+        {getprops !== null &&
+          getprops.map((val) => (
+            <div
+              key={val.idMeal}
+              className="flex flex-col items-center max-w-sm rounded overflow-hidden 
+          shadow-md shadow-gray-600 m-4 
+          transition-transform duration-300 ease-in-out hover:scale-105"
+            >
+              {/* <a href="#" onClick={() => selectedFun(category.strCategory)}>
+              <img src={category.strCategoryThumb} className="w-full" />
+            </a> */}
+              <img
+                src={val.strMealThumb}
+                alt=""
+                className="w-full"
+                style={{ height: "125px", objectFit: "cover" }}
+              />
+              <div className="px-6 py-4 ">{val.strMeal}</div>
+            </div>
+          ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default MealsAcCat
+export default MealsAcCat;
--- src/api/RecipieExample.jsx
+++ src/api/RecipieExample.jsx
@@ -1,19 +1,13 @@
 import React, { useEffect, useState } from "react";
 import RecipeAPI from "./RecipeApi";
-const { API, API2 } = RecipeAPI;
+import { Link } from "react-router-dom";
+const { API } = RecipeAPI;
 const RecipieExample = () => {
   const [dat, setDat] = useState([]);
-  const [selected, setSelected] = useState(null);
-  const [dab, setDab] = useState([]);
 
   const getCategories = async () => {
     try {
       const response = await API.get();
-      if (selected) {
-        const resp = await API2.get(`?c=${selected}`);
-        setDab(resp.data.meals);
-      }
-
       setDat(response.data.categories);
     } catch (e) {
       console.log(e);
@@ -22,37 +16,31 @@ const RecipieExample = () => {
 
   useEffect(() => {
     getCategories();
-  }, [selected]);
-
-  const selectedFun = (value) => {
-    setSelected(value);
-    console.log(value);
-  };
+  }, []);
 
   return (
-    <div>
+    <div className="px-4 m-3">
       <div className="grid  md:grid-cols-6 gap-2">
         {dat.map((category) => (
           <div
             key={category.idCategory}
             className="flex flex-col items-center max-w-sm rounded overflow-hidden 
-            shadow-md shadow-purple-500 m-4 
+            shadow-md shadow-gray-600 m-4 
             transition-transform duration-300 ease-in-out hover:scale-105"
           >
-            <a href="#" onClick={() => selectedFun(category.strCategory)}>
-              <img src={category.strCategoryThumb} className="w-full" />
-            </a>
+            <Link to={`/category/${category.strCategory}`} className="">
+              <img src={category.strCategoryThumb} alt="" className="w-full" />
+            </Link>
             <div className="px-6 py-4 ">
-              <a href="#" onClick={() => selectedFun(category.strCategory)}>
+              <Link to={`/category/${category.strCategory}`} className="block">
                 {category.strCategory}
-              </a>
-              <div className="font-bold text-xl mb-2"></div>
+              </Link>
             </div>
           </div>
         ))}
       </div>
 
-      <div>
+      {/* <div>
         {dab !== null &&
           dab.map((byCategory) => (
             <div key={byCategory.idMeal}>
@@ -60,7 +48,7 @@ const RecipieExample = () => {
               <p>{byCategory.strMeal}</p>
             </div>
           ))}
-      </div>
+      </div> */}
     </div>
   );
 };
